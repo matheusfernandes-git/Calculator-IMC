2d05b7b41aec6780eae9eec238c2a690b580b609
Matheus Fernandes <100592742+matheusfernandes-git@users.noreply.github.com>
2022-12-21 18:03:44 -0300

    input limpar

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function calcule(){
     let height = document.getElementById('height').value / 100;
     let weight = document.getElementById('weight').value;
 
-    let imc = (weight / (height ** 2)).toFixed(2)
+    let imc = (weight / (height * height)).toFixed(1);
     let res = ''
     if(height == "" || weight == ""){
         alert('ERRO, Digite sua altura e idade corretamente!')
-    }else if(imc < 18.5){
+    }else if(imc < 18.56){
         res =`Seu IMC é ${imc}`;
     }else if(imc < 24.9){
         res =`Seu IMC é ${imc}`;
@@ -18,4 +18,10 @@ function calcule(){
         res =`Seu IMC é ${imc}`;
     }
     document.getElementById('res').innerText= res
+}
+
+function limpar(){
+    document.getElementById('height').value = ''
+    document.getElementById('weight').value = ''
+    document.getElementById('res').value = ''
 }
