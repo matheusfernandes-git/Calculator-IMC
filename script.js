bc35ab64d1b9b9bae0e7ac907818005777e24ac2
Matheus Fernandes <100592742+matheusfernandes-git@users.noreply.github.com>
2023-02-09 19:11:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,25 +3,31 @@ function calculate(){
     let weight = document.getElementById('weight').value;
 
     let imc = (weight / (height * height));
+    imc = `${imc.toFixed(2)}`;
     let res = document.getElementById('res');
-    res.innerHTML = `Seu IMC é: ${imc.toFixed(2)}`;
 
     if(height == "" || weight == ""){
-        alert('ERRO, Digite sua altura e idade corretamente!')
+        alert('ERRO, Digite sua altura e idade corretamente!');
     }else if(imc < 18.56){
-        res =`Seu IMC é ${imc}`;
+        res.innerHTML =`Seu IMC é ${imc} 
+MAGREZA`;
     }else if(imc < 24.9){
-        res =`Seu IMC é ${imc}`;
+        res.innerHTML =`Seu IMC é ${imc}
+NORMAL`;
+    }else if(imc < 29.9){
+        res.innerHTML =`Seu IMC é ${imc}
+SOBREPESO`;
     }else if(imc < 39.9){
-        res =`Seu IMC é ${imc}`;
+        res.innerHTML =`Seu IMC é ${imc}
+OBESIDADE`;
     }else{
-        res =`Seu IMC é ${imc}`;
+        res.innerHTML = `Seu IMC é ${imc}
+OBESIDADE GRAVE`;
     }
-
-    const clear = document.getElementById('limpar');
-    clear.addEventListener('click', () =>{
-        document.getElementById('height').value = ''
-        document.getElementById('weight').value = ''
-        document.getElementById('res').value = ''
-    })
 }
+
+const clear = document.getElementById('limpar')
+clear.addEventListener('click', () =>{
+document.getElementById('height').value = ''
+document.getElementById('weight').value = ''
+})
